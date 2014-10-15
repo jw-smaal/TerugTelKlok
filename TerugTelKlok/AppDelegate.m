42c878b142564b8fb4addce44a2d2945d81b5b28
jw-smaal <usenet@gispen.org>
2014-10-15 20:47:03 +0200

    gfhh

--- TerugTelKlok/AppDelegate.m
+++ TerugTelKlok/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  TerugTelKlok
 //
-//  Created by J SMAAL on 27-12-11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by J SMAAL on 27-12-14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 //
 
 #import "AppDelegate.h"
